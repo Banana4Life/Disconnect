264e77a8643cbe46d6ebc7b5151d6b4c7248ec20
Phillip Schichtel <phillip.public@schich.tel>
2014-08-23 01:43:00 +0200

    init

---
+++ desktop/src/de/cubeisland/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.cubeisland.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.cubeisland.games.Ludumdare30;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Ludumdare30(), config);
+	}
+}
