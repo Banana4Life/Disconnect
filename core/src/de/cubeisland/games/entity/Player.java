96bfeac091ecf13b18602830c5e53ab14f6c5f91
Phillip Schichtel <phillip.public@schich.tel>
2014-08-23 15:12:33 +0200

    simple player input

--- core/src/de/cubeisland/games/entity/Player.java
+++ core/src/de/cubeisland/games/entity/Player.java
@@ -1,18 +1,17 @@
 package de.cubeisland.games.entity;
 
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import de.cubeisland.games.Camera;
 import de.cubeisland.games.DisconnectGame;
 
+import static com.badlogic.gdx.Input.Keys;
 import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled;
 
 public class Player extends Entity {
 
-    @Override
-    public void onSpawn() {
-        getVelocity().set(10, 10);
-    }
+    private float speed = 20;
 
     @Override
     public void render(DisconnectGame game, float delta) {
@@ -21,6 +20,7 @@ public class Player extends Entity {
         r.setColor(Color.RED);
         r.circle(pos.x, pos.y, 8);
         r.end();
+        game.getInputMultiplexer().addProcessor(new Input());
     }
 
     @Override
@@ -30,5 +30,43 @@ public class Player extends Entity {
         Camera camera = game.getCamera();
         camera.position.set(this.pos.x, this.pos.y, 0);
         camera.update();
+    }
+
+    private final class Input extends InputAdapter {
+        @Override
+        public boolean keyDown(int keycode) {
+            switch (keycode) {
+                case Keys.LEFT:
+                    velocity.set(-speed, velocity.y);
+                    return true;
+                case Keys.RIGHT:
+                    velocity.set(speed, velocity.y);
+                    return true;
+                case Keys.UP:
+                    velocity.set(velocity.x, -speed);
+                    return true;
+                case Keys.DOWN:
+                    velocity.set(velocity.x, speed);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public boolean keyUp(int keycode) {
+            switch (keycode) {
+                case Keys.LEFT:
+                case Keys.RIGHT:
+                    velocity.set(0, velocity.y);
+                    return true;
+                case Keys.UP:
+                case Keys.DOWN:
+                    velocity.set(velocity.x, 0);
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
