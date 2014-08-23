a13f9413b2c787cfd6272d6effcbd7311ae64709
Phillip Schichtel <phillip.public@schich.tel>
2014-08-23 15:33:26 +0200

    better input

--- core/src/de/cubeisland/games/entity/Player.java
+++ core/src/de/cubeisland/games/entity/Player.java
@@ -11,7 +11,7 @@ import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.Filled;
 
 public class Player extends Entity {
 
-    private float speed = 20;
+    private static final float SPEED = 60;
 
     @Override
     public void render(DisconnectGame game, float delta) {
@@ -37,16 +37,16 @@ public class Player extends Entity {
         public boolean keyDown(int keycode) {
             switch (keycode) {
                 case Keys.LEFT:
-                    velocity.set(-speed, velocity.y);
+                    velocity.set(-SPEED, velocity.y);
                     return true;
                 case Keys.RIGHT:
-                    velocity.set(speed, velocity.y);
+                    velocity.set(SPEED, velocity.y);
                     return true;
                 case Keys.UP:
-                    velocity.set(velocity.x, -speed);
+                    velocity.set(velocity.x, -SPEED);
                     return true;
                 case Keys.DOWN:
-                    velocity.set(velocity.x, speed);
+                    velocity.set(velocity.x, SPEED);
                     return true;
                 default:
                     return false;
@@ -55,18 +55,23 @@ public class Player extends Entity {
 
         @Override
         public boolean keyUp(int keycode) {
-            switch (keycode) {
-                case Keys.LEFT:
-                case Keys.RIGHT:
-                    velocity.set(0, velocity.y);
-                    return true;
-                case Keys.UP:
-                case Keys.DOWN:
-                    velocity.set(velocity.x, 0);
-                    return true;
-                default:
-                    return false;
+            if (keycode == Keys.LEFT && velocity.x < 0) {
+                velocity.x = 0;
+                return true;
             }
+            if (keycode == Keys.RIGHT && velocity.x > 0) {
+                velocity.x = 0;
+                return true;
+            }
+            if (keycode == Keys.UP && velocity.y < 0) {
+                velocity.y = 0;
+                return true;
+            }
+            if (keycode == Keys.DOWN && velocity.y > 0) {
+                velocity.y = 0;
+                return true;
+            }
+            return false;
         }
     }
 }
