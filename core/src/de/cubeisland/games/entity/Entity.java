03898ea2e9dd464164dc68ee9aee667a1c7c1ebc
Anselm Brehme <Faithcaio@cubeisland.de>
2014-08-24 10:54:47 +0200

    fixing collisions?

--- core/src/de/cubeisland/games/entity/Entity.java
+++ core/src/de/cubeisland/games/entity/Entity.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import de.cubeisland.games.DisconnectGame;
 import de.cubeisland.games.World;
+import de.cubeisland.games.entity.collision.Collider;
 import de.cubeisland.games.entity.collision.CollisionBox;
 
 public abstract class Entity {
@@ -24,17 +25,19 @@ public abstract class Entity {
     }
 
     public void render(DisconnectGame game, float delta) {
-//        if (this.collisionBox != null) {
-//            ShapeRenderer r = getWorld().getCamera().getShapeRenderer();
-//
-//            final float firstX = pos.x + collisionBox.getOffsetX();
-//            final float firstY = pos.y - collisionBox.getOffsetY();
-//
-//            r.begin(ShapeRenderer.ShapeType.Filled);
-//            r.setColor(this instanceof TileEntity ? Color.BLUE : Color.RED);
-//            r.rect(firstX, firstY, collisionBox.getWidth(), collisionBox.getHeight());
-//            r.end();
-//        }
+        /* CollisionBoxes
+        if (this.collisionBox != null) {
+            ShapeRenderer r = getWorld().getCamera().getShapeRenderer();
+
+            final float firstX = pos.x + collisionBox.getOffsetX();
+            final float firstY = pos.y - collisionBox.getOffsetY();
+
+            r.begin(ShapeRenderer.ShapeType.Filled);
+            r.setColor(this instanceof TileEntity ? Color.BLUE : Color.RED);
+            r.rect(firstX, firstY, collisionBox.getWidth(), collisionBox.getHeight());
+            r.end();
+        }
+        //*/
     }
 
     public void onSpawn() {
@@ -51,38 +54,70 @@ public abstract class Entity {
     }
 
     public void onTileCollide(TileEntity tile, Rectangle collisionBox) {
-        if (collisionBox.width > collisionBox.height) {
-            if (!onTileCollideY(collisionBox)) {
-                onTileColideX(collisionBox);
+        float posy = pos.y;
+        float posx = pos.x;
+        if (Math.abs(lastPos.y - pos.y) > Math.abs((lastPos.x - pos.x)))
+        {
+            if (onTileCollideY(collisionBox))
+            {
+                collisionBox = Collider.findCollision(this, tile);
             }
-        } else {
-            if (!onTileColideX(collisionBox)) {
-                onTileCollideY(collisionBox);
+            if (collisionBox != null)
+            {
+                pos.y = posy;
+                onTileColideX(collisionBox);
+                collisionBox = Collider.findCollision(this, tile);
+                if (collisionBox != null)
+                {
+                    pos.x = posx;
+                    System.out.println("UNRESOLVED COLLISION! " + collisionBox.height + " " + collisionBox.width);
+                }
             }
         }
+        else
+        {
+            if (onTileColideX(collisionBox))
+            {
+                collisionBox = Collider.findCollision(this, tile);
+            }
+            if (collisionBox != null)
+            {
+                pos.x = posx;
+                onTileCollideY(collisionBox);
+                collisionBox = Collider.findCollision(this, tile);
+                if (collisionBox != null)
+                {
+                    pos.y = posy;
+                    System.out.println("UNRESOLVED COLLISION! " + collisionBox.height + " " + collisionBox.width);
+                }
+            }
+        }
+
     }
 
     private boolean onTileCollideY(Rectangle collisionBox) {
         float newVal;
         if (velocity.y > 0) {
             newVal = collisionBox.y - this.getCollisionBox().getHeight() - this.getCollisionBox().getOffsetY();
-            if (newVal >= lastPos.y) {
+            if (newVal >= lastPos.y && newVal < pos.y) {
                 pos.y = newVal;
                 return true;
             }
             else
             {
                 pos.y = lastPos.y;
+                return true;
             }
         } else if (velocity.y < 0) {
             newVal = collisionBox.y + collisionBox.height - this.getCollisionBox().getOffsetY();
-            if (newVal <= lastPos.y) {
+            if (newVal <= lastPos.y && newVal > pos.y) {
                 pos.y = newVal;
                 return true;
             }
             else
             {
                 pos.y = lastPos.y;
+                return true;
             }
         }
         return false;
@@ -92,23 +127,25 @@ public abstract class Entity {
         float newVal;
         if (velocity.x > 0) {
             newVal = collisionBox.x - this.getCollisionBox().getWidth() - this.getCollisionBox().getOffsetX();
-            if (newVal >= lastPos.x) {
+            if (newVal >= lastPos.x && newVal < pos.x) {
                 pos.x = newVal;
                 return true;
             }
             else
             {
                 pos.x = lastPos.x;
+                return true;
             }
         } else if (velocity.x < 0) {
             newVal = collisionBox.x + collisionBox.width - this.getCollisionBox().getOffsetX();
-            if (newVal <= lastPos.x) {
+            if (newVal <= lastPos.x && newVal > pos.x) {
                 pos.x = newVal;
                 return true;
             }
             else
             {
                 pos.x = lastPos.x;
+                return true;
             }
         }
         return false;
