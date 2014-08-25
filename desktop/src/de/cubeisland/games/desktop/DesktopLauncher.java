2d79ab967f34cb4398212ae591fc0db3b55f82bd
JonasDann <jonas.chr.dann@gmail.com>
2014-08-26 01:38:04 +0200

    Added icons and renamed the window to disconnect

--- desktop/src/de/cubeisland/games/desktop/DesktopLauncher.java
+++ desktop/src/de/cubeisland/games/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package de.cubeisland.games.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.cubeisland.games.DisconnectGame;
@@ -9,6 +10,9 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.vSyncEnabled = false;
         config.foregroundFPS = 300;
+        config.addIcon("icons/icon16.png", Files.FileType.Internal);
+        config.addIcon("icons/icon32.png", Files.FileType.Internal);
+        config.title = "Disconnect";
 		new LwjglApplication(new DisconnectGame(), config);
 	}
 }
